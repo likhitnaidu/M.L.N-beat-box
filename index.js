489aefdadb1841a904944eb74c5a302b6970e0d8
M.Likhit Naidu <71972560+likhitnaidu@users.noreply.github.com>
2021-10-31 17:20:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,34 @@ let myaudio2=document.querySelector('#audio2');
 soudBtn.addEventListener('click',()=>{
     myaudio2.play();
 });
-const sondBtn=document.querySelector('#c');
+const soandBtn=document.querySelector('#c');
 let myaudio3=document.querySelector('#audio3');
-sondBtn.addEventListener('click',()=>{
+soandBtn.addEventListener('click',()=>{
     myaudio3.play();
 });
+const loundBtn=document.querySelector('#d');
+let myaudio4=document.querySelector('#audio4');
+loundBtn.addEventListener('click',()=>{
+    myaudio4.play();
+});
+const lounBtn=document.querySelector('#e');
+let myaudio5=document.querySelector('#audio5');
+lounBtn.addEventListener('click',()=>{
+    myaudio5.play();
+});
+const loudBtn=document.querySelector('#f');
+let myaudio6=document.querySelector('#audio6');
+loudBtn.addEventListener('click',()=>{
+    myaudio6.play();
+});
+const lndBtn=document.querySelector('#g');
+let myaudio7=document.querySelector('#audio7');
+lndBtn.addEventListener('click',()=>{
+    myaudio7.play()
+});
+const oundBtn=document.querySelector('#h');
+let myaudio8=document.querySelector('#audio8');
+oundBtn.addEventListener('click',()=>{
+    myaudio8.play();
+});
+
