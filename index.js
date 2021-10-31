8c8c7e6ad4adde719dd77e46dcadac5aeefa2ca1
M.Likhit Naidu <71972560+likhitnaidu@users.noreply.github.com>
2021-10-31 13:37:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const soundBtn=document.querySelector('#a');
 let myaudio1=document.querySelector('#audio1');
 soundBtn.addEventListener('click',()=>{
     myaudio1.play();
-})
+});
+const soudBtn=document.querySelector('#b');
+let myaudio2=document.querySelector('#audio2');
+soudBtn.addEventListener('click',()=>{
+    myaudio2.play();
+});
+const sondBtn=document.querySelector('#c');
+let myaudio3=document.querySelector('#audio3');
+sondBtn.addEventListener('click',()=>{
+    myaudio3.play();
+});
